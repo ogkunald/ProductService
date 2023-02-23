f6128ebdcb6cdba5362d988e6c8b3f610498134c
ogkunald <kunaldongare111@gmail.com>
2023-02-23 12:44:36 +0000

    Product Service setup ready

---
+++ src/main/java/com/nastyzera/ProductService/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.nastyzera.ProductService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nastyzera.ProductService.model.ProductRequest;
+import com.nastyzera.ProductService.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    
+    @Autowired
+    private ProductService productService;
+
+    // @PreAuthorize("hasAuthority('Admin')")
+    @PostMapping
+    public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+        long productId = productService.addProduct(productRequest);
+        return new ResponseEntity<>(productId, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/nastyzera/ProductService/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.nastyzera.ProductService.service;
+
+import com.nastyzera.ProductService.model.ProductRequest;
+
+public interface ProductService {
+
+    long addProduct(ProductRequest productRequest);
+    
+}
