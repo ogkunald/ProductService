7decd3a72f8a35340cf3fc195d724a1562b025ac
ogkunald <kunaldongare111@gmail.com>
2023-03-11 07:17:02 +0000

    okta authentication/authorization added in Product Service

--- src/main/java/com/nastyzera/ProductService/controller/ProductController.java
+++ src/main/java/com/nastyzera/ProductService/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.nastyzera.ProductService.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,14 +24,14 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    // @PreAuthorize("hasAuthority('Admin')")
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
         long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
-    //@PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer') || hasAuthority('SCOPE_internal')")
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer') || hasAuthority('SCOPE_internal')")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
         ProductResponse productResponse
